f132c66af4cd573523f07d17f1ee0debd08e96f4
Sanju Devadas <sanjudevadas11@gmail.com>
2025-03-31 08:39:50 +0530

    routes

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about',function(){
+Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/product/{id}', function(string $id){
-return "Product ID=$id";
+Route::get('/product/{id}', function (string $id) {
+    return "Product ID = $id";
 });
+
+Route::get('/service/{id}/{id2}', function (int $id, int $id2) {
+    return "Sum = " . ($id + $id2);
+});
+
+Route::get('/sum/{a}/{b}',function(float $a, float $b){
+
+    return $a+ $b;
+
+})->whereNumber(['a','b']);
+
